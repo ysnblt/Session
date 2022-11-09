7b45611045d1dbb836233a2d35c6fd17f20bd825
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-09 09:08:14 +0300

    Add project files.

---
+++ Session/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Session.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //tarayıcıya ekliyor
+            var s = new Student() /*{Id = 1 , Name = "Yasin" , Email = "ysnblt.gmail.com" }*/;
+            s.Name = "Yasin";
+            s.Id = 1;
+            s.Email = "ysnblt.gmail.com";
+            // serialize c# değerini json'a çevirir
+            HttpContext.Session.SetString("User", JsonConvert.SerializeObject(s));
+            return View(s);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Session/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Session.Models;
+
+namespace Session.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            // home controllerda tarayıcıya kayıt edilen bilgiyi okuyor.
+            var s = JsonConvert.DeserializeObject<Student>(HttpContext.Session.GetString("User"));
+            return View(s);
+        }
+    }
+}
